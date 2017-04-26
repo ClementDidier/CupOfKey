5b0246894dc74e2c6f8d31521d6dd92f0612f963
magelion <www.max87@yahoo.fr>
2017-04-26 18:20:22 +0200

    structure pour saisie commande en console

--- src/main/java/devops/cupofkey/Program.java
+++ src/main/java/devops/cupofkey/Program.java
@@ -1,23 +1,85 @@
 package devops.cupofkey;
 
 import java.io.IOException;
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
 
 import devops.cupofkey.client.Client;
 
 public class Program
 {
     public static void main( String[] args )
-    {  	
-        System.out.println( "Hello World!" );
-        
+    {  
         try (Client client = new Client("localhost"))
         {
-        	client.store("myKey", "MyString");
-        	client.remove("myKey");
+        	Scanner scan = new Scanner(System.in);
+        	
+        	StringBuilder build = new StringBuilder();
+        	
+        	build.append("CupofKey client \n");
+        	build.append("Commanndes possibles : \n");
+        	build.append("get, set, exist, incr, mult \n");    	
+        	System.out.println(build.toString());
+        	
+        	String cmd; 
+        	while((cmd = scan.nextLine())!="quit"){
+        		String[] linecut = cmd.split(" ");
+        		switch(linecut[0]){
+        		case "set" :
+        			if (linecut.length==2){
+        				
+        			}else if(linecut.length==3){
+        				
+        			}else{
+        				
+        			}
+        			break;
+        		case "get" :
+        			if (linecut.length==2){
+        			}else if(linecut.length==3){
+    				
+        			}else{
+    				
+        			}
+        			break;
+        		case "incr" :
+        			if (linecut.length==2){
+        			}else if(linecut.length==3){
+    				
+        			}else{
+    				
+        			}
+        			break;
+        		case "exist":
+        			if (linecut.length==2){
+        			}else if(linecut.length==3){
+    				
+        			}else{
+    				
+        			}
+        			break;
+        		case "mult" :
+        			if (linecut.length==2){
+        			}else if(linecut.length==3){
+    				
+        			}else{
+    				
+        			}
+        			break;
+        		default :
+        			
+        		}
+        	}
+        	
         } 
         catch (IOException e) 
         {
 			System.err.println(e.getMessage());
 		}
+    	
+        //System.out.println( "Hello World!" );
     }
+
+
 }
